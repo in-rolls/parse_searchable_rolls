4823dc32d2c6e2811f25726408fa53aa7d03dfc4
Gaurav Sood <soodoku@users.noreply.github.com>
2018-03-05 07:42:00 -0500

    add parl_constituency

--- pdfparser/globals.py
+++ pdfparser/globals.py
@@ -8,8 +8,8 @@ Parse PDF Indian electoral rolls to a CSV.'''
 APP_CMD = APP_NAME.lower()
 APP_PATH = os.path.dirname(__file__)
 with open(os.path.join(APP_PATH, 'VERSION'), 'r') as f:
-	APP_VERSION = f.read().strip()
-	APP_NAME_WITH_VERSION = ' '.join([APP_NAME, APP_VERSION]).strip()
+    APP_VERSION = f.read().strip()
+    APP_NAME_WITH_VERSION = ' '.join([APP_NAME, APP_VERSION]).strip()
 
 # Log settings
 LOG_FILE = os.path.join(APP_PATH, 'logs/{}.log'.format(APP_NAME.lower()))
@@ -22,8 +22,8 @@ TRACK_FILE_NAME = '.{}'.format(APP_NAME.lower())
 OUTPUT_FILE_TS = '%Y%m%d-%H%M%S'
 OUTPUT_FILE = os.path.join(APP_PATH, 'output/Parsed-{}.csv')
 OutputRow = namedtuple(
-	'OutputRow',
-	'number id elector_name father_or_husband_name has_husband house_no age sex ac_name part_no year state filename \
-main_town police_station mandal revenue_division district pin_code polling_station_name polling_station_address \
-net_electors_male net_electors_female net_electors_third_gender net_electors_total'
+    'OutputRow',
+    'number id elector_name father_or_husband_name has_husband house_no age sex ac_name parl_constituency part_no year \
+state filename main_town police_station mandal revenue_division district pin_code polling_station_name \
+polling_station_address net_electors_male net_electors_female net_electors_third_gender net_electors_total'
 )
