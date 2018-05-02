d5ff02d842916130809bba73ddebc20c1446a2ea
***** <gsood07@gmail.com>
2018-05-02 15:25:48 -0400

    updated args

--- pdfparser/modules/args.py
+++ pdfparser/modules/args.py
@@ -1,58 +1,73 @@
 from argparse import ArgumentParser
-from .rolls import ALL_STATES
+from .rolls import states
 from helpers import timestamp, ispdf, isfile, isdir, dirname, filext
 from globals import APP_DESC, APP_CMD, APP_NAME_WITH_VERSION, OUTPUT_FILE, OUTPUT_FILE_TS
 
 
 def get_args():
-	parser = ArgumentParser(description=APP_DESC, prog=APP_CMD)
+    parser = ArgumentParser(description=APP_DESC, prog=APP_CMD)
 
-	parser.add_argument('-f', '--file', metavar='FILE', help='path to the specific PDF file to be parsed')
+    parser.add_argument('-f', '--file', metavar='FILE', help='path to the PDF file that is to be parsed')
 
-	parser.add_argument('-d', '--dir', metavar='DIR', help='path to directory containing the PDF files')
+    parser.add_argument('-d', '--dir', metavar='DIR', help='path to the directory containing the PDF files that are to be parsed')
 
-	parser.add_argument('-s', '--state', metavar='STATE', help='Name of state where PDF document(s) is/are published')
+    parser.add_argument('-s', '--state', metavar='STATE', help='state whose PDF rolls(s) are to be parsed')
 
-	parser.add_argument('-o', '--out', metavar='FILE', default=OUTPUT_FILE.format(timestamp(OUTPUT_FILE_TS)), help='\
-Specify the output file for storing the results (must be \'.csv\' file). The default output file is \
-\'Parsed-{timestamp}.csv\' located in \'output\' directory')
+    parser.add_argument('-l', '--lang', metavar='LANG', default='english', help='specify language used in parsing \
+documents (default is English if not being specified)')
 
-	parser.add_argument('--resume', action='store_true', default=False, help='Allows us to resume parsing if \
-the program was stopped unexpectedly or intentionally. Only takes effect if. Only takes effect if directory is being parsed')
+    parser.add_argument('-o', '--out', metavar='FILE', default=OUTPUT_FILE.format(timestamp(OUTPUT_FILE_TS)), help='\
+specify output file for storing parsed result (must be \'.csv\' file). The default output file is \
+\'Parsed-{timestamp}.csv\' and stored in the \'output\' directory')
 
-	parser.add_argument('--version', action='version', version=APP_NAME_WITH_VERSION)
+    parser.add_argument('--resume', action='store_true', default=False, help='allows the parsing to be \
+resumed later if the program is stopped unexpectedly or intentionally. Only takes effect if it is applied to a directory')
 
-	parser.add_argument('--all-states', action='store_true', default=False, help='show all supported states and exit')
+    parser.add_argument('--version', action='version', version=APP_NAME_WITH_VERSION)
 
-	args = parser.parse_args()
+    parser.add_argument('--all-states', action='store_true', default=False, help='show all the states that are supported and exit')
 
-	if args.all_states:
-		parser.exit(message='States supported: %s\n' % ', '.join(ALL_STATES))
+    args = parser.parse_args()
 
-	if not args.file and not args.dir or not args.state:
-		parser.error('the following arguments are required: -f/--file or -d/--dir, -s/--state')
+    if args.all_states:
+        out = []
+        chars = max(len(x) for x in states.keys())
+        for state, langs in states.items():
+            out.append('%-{}s : %s'.format(chars) % (state, ', '.join(langs)))
+        parser.exit(message='States supported:\n%s\n' % '\n'.join(out))
 
-	if args.file and args.dir:
-		parser.error('only accepts one of --file or --dir at a time')
+    if not args.file and not args.dir or not args.state:
+        parser.error('the following arguments are required: -f/--file or -d/--dir, -s/--state')
 
-	elif args.file:
-		if not isfile(args.file):
-			parser.error('file not found: %s' % args.file)
-		if not ispdf(args.file):
-			parser.error('file must be in PDF format')
-		if args.resume:
-			parser.error('parsing file does not allow --resume')
+    if args.file and args.dir:
+        parser.error('only accepts one of --file or --dir at a time')
 
-	elif args.dir:
-		if not isdir(args.dir):
-			parser.error('folder not found: %s' % args.dir)
+    elif args.file:
+        if not isfile(args.file):
+            parser.error('file not found: %s' % args.file)
+        if not ispdf(args.file):
+            parser.error('file must be in PDF format')
+        if args.resume:
+            parser.error('parsing file does not allow --resume')
 
-	if dirname(args.out) and not isdir(dirname(args.out)):
-		parser.error('output folder does not exist: %s' % dirname(args.out))
+    elif args.dir:
+        if not isdir(args.dir):
+            parser.error('folder not found: %s' % args.dir)
 
-	if filext(args.out).lower() != 'csv':
-		parser.error('output file must be \'.csv\'')
+    if args.state.lower() not in states:
+        parser.error('unsupported state \'%s\'' % args.state)
 
-	args.path = args.file or args.dir
+    if args.lang and args.lang.lower() not in states[args.state.lower()]:
+        parser.error('state \'%s\' does not have \'%s\' language' % (args.state, args.lang))
 
-	return args
+    if dirname(args.out) and not isdir(dirname(args.out)):
+        parser.error('output folder does not exist: %s' % dirname(args.out))
+
+    if filext(args.out).lower() != 'csv':
+        parser.error('output file must be \'.csv\'')
+
+    args.path = args.file or args.dir
+    args.state = args.state.lower()
+    args.lang = args.lang.lower()
+
+    return args
